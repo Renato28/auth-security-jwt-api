daacb00ac9f60aac58fe31535a8d7057557f33c8
Renato28 <renatonobrega87@gmail.com>
2024-12-26 15:06:10 -0300

    criando projeto

---
+++ src/test/java/io/github/renatonobrega/auth_security_jwt_api/AuthSecurityJwtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renatonobrega.auth_security_jwt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityJwtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
