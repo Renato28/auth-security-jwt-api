ca44d8ec97f18affe81d2a598c262735bb71e50a
Renato28 <renatonobrega87@gmail.com>
2024-12-26 15:19:36 -0300

    Implementando UserModel e UserRole

---
+++ src/main/java/io/github/renatonobrega/auth_security_jwt_api/user/model/UserModel.java
@@ -0,0 +1,81 @@
+package io.github.renatonobrega.auth_security_jwt_api.user.model;
+
+import io.github.renatonobrega.auth_security_jwt_api.user.model.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String email;
+    private String password;
+    private UserRole userRole;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private Date createAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_at", nullable = true)
+    private Date updateAt;
+
+    public UserModel(String email, String password, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.userRole == userRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
